35f04f403b481948e6eb3b56127943ab229d03d7
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2022-01-21 10:27:57 +0100

    Pocket Dictionary Iteration

--- Pocket Dictionary/main.py
+++ Pocket Dictionary/main.py
@@ -4,5 +4,7 @@ dict = PyDictionary()
 
 print("Welcome to my Pocket Dictionary")
 
-query = input("What do you want to Search: ")
+query = input("What do you want to Search ? ")
+# for key, value  in dict.meaning(query).items():
+#      print(key, '->', value)
 print(f'The result for meaning search of "{query}" is : {dict.meaning(query)}')
